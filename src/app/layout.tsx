a59297a5ac87edd23fc62bb7c941576063569728
Mojib Rsm <mojibrsm@gmail.com>
2025-09-06 17:43:15 +0000

    <meta name="google-adsense-account" content="ca-pub-8850298223242719">

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
+        <meta name="google-adsense-account" content="ca-pub-8850298223242719" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8850298223242719"
